7b6571d358bb3742b5521872bed8f956693ec5c0
jacky2252v <vivekbaraiya000@gmail.com>
2025-02-28 12:34:15 +0530

    feat: update layout and styling in Body component for improved visual hierarchy and responsiveness

--- src/components/NewLang/body.tsx
+++ src/components/NewLang/body.tsx
@@ -5,31 +5,28 @@ import LanguageSelector from "./LanguageSelector";
 
 export default function Body() {
     return (
-        <div className="flex justify-center items-center text-center min-h-screen">
-            <div className="text-center">
-                <div className="mb-48">
-                    <Image
-                        src="/img/openNext.png"
-                        alt="LOGO"
-                        width={384} // w-96 (384px)
-                        height={192} // Adjust height accordingly
-                        className="hover:opacity-75 mx-auto"
-                        priority
-                    />
+        <div className="flex flex-col justify-center items-center text-center min-h-screen">
+            <div className="mb-44">
+                <Image
+                    src="/img/openNext.png"
+                    alt="LOGO"
+                    width={350}
+                    height={150}
+                    className="hover:opacity-75 mx-auto"
+                    priority
+                />
+                <h1 className="p-8 pb-5 font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">
+                    Build Faster, Smarter, Better -<br />
+                    A
+                    <span className="bg-gradient-to-r from-yellow-400 to-orange-600 bg-clip-text text-transparent">
+                        {' '} Lightning-Fast {' '}
+                    </span>
+                    CMS for the Modern Web.
+                </h1>
 
-                    <h1 className="p-12 font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">
-                        Build Faster, Smarter, Better -<br />
-                        A
-                        <span className="bg-gradient-to-r from-yellow-400 to-orange-600 bg-clip-text text-transparent">
-                            {' '} Lightning-Fast {' '}
-                        </span>
-                        CMS for the Modern Web.
-                    </h1>
-
-                    <div className="w-full h-56 max-w-2xl mx-auto p-6 bg-white rounded-2xl shadow-xl flex flex-col items-center justify-center">
-                        <h2>Start Installation</h2>
-                        <LanguageSelector />
-                    </div>
+                <div className="w-full h-80 max-w-2xl mb-3 mx-auto p-6 bg-white rounded-2xl shadow-xl flex flex-col items-center justify-center">
+                    <h2 className="font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">Start Installation</h2>
+                    <LanguageSelector />
                 </div>
             </div>
         </div>
