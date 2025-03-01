1fed01afb17576e08c32564ec4e949655069ec25
jacky2252v <vivekbaraiya000@gmail.com>
2025-03-01 17:28:57 +0530

    style: adjust layout and typography in NewLang components for improved aesthetics

--- src/components/NewLang/body.tsx
+++ src/components/NewLang/body.tsx
@@ -5,17 +5,17 @@ import LanguageSelector from "./LanguageSelector";
 
 export default function Body() {
     return (
-        <div className="flex flex-col justify-center items-center text-center min-h-screen">
-            <div className="mb-44">
+        <div className="flex flex-col justify-center items-center text-center">
+            <div className="absolute top-4">
                 <Image
                     src="/img/openNext.png"
                     alt="LOGO"
-                    width={350}
-                    height={150}
+                    width={150}
+                    height={50}
                     className="hover:opacity-75 mx-auto"
                     priority
                 />
-                <h1 className="p-8 pb-5 font-extrabold sm:text-2xl md:text-3xl lg:text-4xl">
+                <h1 className="p-8 pb-5 font-extrabold sm:text-xl md:text-2xl lg:text-xl">
                     Build Faster, Smarter, Better -<br />
                     A
                     <span className="bg-gradient-to-r from-yellow-400 to-orange-600 bg-clip-text text-transparent">
@@ -23,9 +23,8 @@ export default function Body() {
                     </span>
                     CMS for the Modern Web.
                 </h1>
-
-                <div className="w-full h-80 max-w-2xl mb-3 mx-auto p-6 bg-white rounded-2xl shadow-xl flex flex-col items-center justify-center">
-                    <h2 className="font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">Start Installation</h2>
+                <div className="w-full h-96 max-w-2xl mb-3 mx-auto p-6 bg-white rounded-2xl shadow-xl flex flex-col items-center justify-center mt-4">
+                    <h2 className="font-extrabold sm:text-3xl md:text-4xl lg:text-3xl">Start Installation</h2>
                     <LanguageSelector />
                 </div>
             </div>
