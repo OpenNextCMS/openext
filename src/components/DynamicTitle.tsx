f7a5e1cf0da314dd2011f1cdb46730a56a66af6a
jacky2252v <vivekbaraiya000@gmail.com>
2025-03-20 11:52:21 +0530

    DynamicTitle: bug fixes for build

--- src/components/DynamicTitle.tsx
+++ src/components/DynamicTitle.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function DynamicTitle() {
     const [title, setTitle] = useState("Loading...");
-    const [desc, setDesc] = useState("Loading...");
+    // const [desc, setDesc] = useState("Loading...");
 
     useEffect(() => {
         async function fetchTitle() {
@@ -13,11 +13,11 @@ export default function DynamicTitle() {
                 const res = await fetch(`${backendUrl}/api/dashboard/settings`);
                 const data = await res.json();
                 setTitle(data?.data?.settings?.siteTitle || "Next.js Setup Project");
-                setDesc(data?.data?.settings?.description || "Comprehensive Next.js Project Setup");
+                // setDesc(data?.data?.settings?.description || "Comprehensive Next.js Project Setup");
             } catch (error) {
                 console.error("Failed to fetch site title:", error);
                 setTitle("Next.js Setup Project");
-                setDesc("Comprehensive Next.js Project Setup");
+                // setDesc("Comprehensive Next.js Project Setup");
             }
         }
 
